80d1d0551b631ff696a5c6c6da59b2ccbb414689
Mikudo39 <1350995183@qq.com>
2025-06-11 21:57:41 +0800

    修复了一些警告并删除了多余的文件

--- takeout/src/main/java/com/blm/takeout/service/impl/ReviewServiceImpl.java
+++ takeout/src/main/java/com/blm/takeout/service/impl/ReviewServiceImpl.java
@@ -10,7 +10,6 @@ import com.blm.takeout.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
--- takeout/src/main/java/com/blm/takeout/service/impl/UserServiceImpl.java
+++ takeout/src/main/java/com/blm/takeout/service/impl/UserServiceImpl.java
@@ -14,10 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
