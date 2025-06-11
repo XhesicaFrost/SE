a067931d3a49a1ee19dac7cc0f1bf024ffaa1053
Mikudo39 <1350995183@qq.com>
2025-06-11 21:58:26 +0800

    同上

--- takeout/src/main/java/com/blm/takeout/config/WebConfig.java
+++ takeout/src/main/java/com/blm/takeout/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.blm.takeout.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
